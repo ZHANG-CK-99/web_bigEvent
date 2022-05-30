63df2d0c0bcd6eedd93331e6106519cb593adf5a
zck <1693002443@qq.com>
2022-05-30 20:32:33 +0800

    完成了主页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,21 @@
 $.ajaxPrefilter((options) => {
     options.url = `http://www.liulongbin.top:3007` + options.url;
+    // 为my相关接口 注入 token
+    if(options.url.includes('/my/')) {
+        options.headers = {
+            Authorization: localStorage.getItem("token"),
+        };
+    }
+
+    // 每次请求token是否存在， 或者是否过期
+    options.complete = (res) => {
+        if(res.responseJSON.status === 1 &&
+            res.responseJSON.message === '身份认证失败！'
+        ) {
+            // 强制清空
+            localStorage.removeItem('token');
+            // 2.跳转页面
+            location.href = '/login.html';
+        }
+    }
 })
